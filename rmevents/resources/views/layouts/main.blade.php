ccedfa0691bd620301a758be0b3ea61c2bafb757
RobersonMariani <73360967+RobersonMariani@users.noreply.github.com>
2022-12-01 12:13:12 -0300

    finishstyle

--- rmevents/resources/views/layouts/main.blade.php
+++ rmevents/resources/views/layouts/main.blade.php
@@ -7,6 +7,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="/js/script.js"></script>
